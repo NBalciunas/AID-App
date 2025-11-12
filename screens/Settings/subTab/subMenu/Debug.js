2efb8a2c4336a9ca62c3a8cc20269300b825d7c1
Nojus <balciunasnojus@gmail.com>
2025-11-12 13:42:46 +0100

    overall sioka tokia tvarka jau yra navigatinimuose po puslapius

---
+++ screens/Settings/subTab/subMenu/Debug.js
@@ -0,0 +1,13 @@
+import { View, Text } from "react-native";
+
+const DebugMenu = () => {
+    return(
+        <View>
+            <Text>
+                Debug placeholder
+            </Text>
+        </View>
+    );
+}
+
+export default DebugMenu;
