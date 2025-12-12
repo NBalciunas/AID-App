8428076cc7822cc767e4ac8545407547986c4a07
Nojus <balciunasnojus@gmail.com>
2025-12-12 22:50:39 +0100

    AThardcodinimas kaip ir padarytas

--- assets/maps/index.js
+++
@@ -1,5 +0,0 @@
-export default [
-    { name: "home", data: require("./home.json") },
-    { name: "shop", data: require("./shop.json") },
-    { name: "shop straight", data: require("./shop straight.json") },
-];
